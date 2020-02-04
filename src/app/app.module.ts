e4bdbd2e46c417bb8b1e688d803c97d2332b1555
ashish-mahour <amashishmahour@gmail.com>
2020-02-04 18:19:26 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import {IonicModule} from "@ionic/angular"
+import {IonicModule} from "@ionic/angular";
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, IonicModule.forRoot() ],
